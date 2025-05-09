91a66a4b44fb07c64402ce6b6e9cbc0d02be9bed
Matthew Gemmell <matthewrgemmell@hotmail.com>
2025-05-09 16:05:51 +0100

    mammal sleep brain converted to actual kgs

--- README.md
+++ README.md
@@ -12,4 +12,9 @@ Datastes include:
 
 crab_age_pred_tbl: https://www.kaggle.com/datasets/sidhus/crab-age-prediction
 
+mammal_sleep_tbl: https://www.kaggle.com/datasets/volkandl/sleep-in-mammals
+
+__Note:__ Page for mammal_sleep_tbl says the brain_wt is in kg when it is in grams. 
+The values in the included tibble have been converted to kgs.
+
 Package created with guidance from : https://rstudio4edu.github.io/rstudio4edu-book/data-pkg.html
--- data-raw/mammals.R
+++ data-raw/mammals.R
@@ -8,6 +8,9 @@ library(devtools)
 
 #Read in data as tibble
 mammal_sleep_tbl <- readr::read_csv(file = "data-raw/mammals.csv",
-                                    col_types = "cdddddddfff")
+                                    col_types = "cdddddddfff") |>
+#Convert brain_wt to actual kg
+#Kaggle page says it is in kgs but it is obviously in grams
+                    dplyr::mutate(brain_wt = brain_wt / 1000)
 #Set tibble as a dataset
 usethis::use_data(mammal_sleep_tbl, overwrite = TRUE)
